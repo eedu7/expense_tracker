e5e20f667719b6a82bee101fb4b26e383001e1ec
eedu7 <mueedahmad067@gmail.com>
2024-09-05 09:35:52 +0500

    feat: project completed successfully

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from datetime import datetime
-from math import exp
+import argparse
 
+from numpy import argwhere
 from requests import head
 
 
@@ -109,13 +110,53 @@ class ExpenseHandler:
             s = f"{id.ljust(3, " ")}|{date.center(10, " ")}|{amount.center(10, " ")}|{description.rjust(12, " ")}"
             print(s)
 
-    def summary(self, expenses):
-        total_amount = sum([float(i[2]) for i in expenses])
+    def summary(self, expenses, month=None, day=None, year=None):
+        filtered_expenses = expenses
+        if day:
+            filtered_expenses = self.filter_expenses(expenses, "day", day)
+        elif month:
+            filtered_expenses = self.filter_expenses(expenses, "month", month)
+        elif year:
+            filtered_expenses = self.filter_expenses(expenses, "year", year)
+        total_amount = sum([float(i[2]) for i in filtered_expenses])
         return total_amount
 
 
 def main():
-    expenses = ExpenseHandler()
+    parser = argparse.ArgumentParser(description="Process some files.")
+    parser.add_argument("input", type=str, help="Command")
+    parser.add_argument("--description", type=str, help="Description of the expense")
+    parser.add_argument("--amount", type=str, help="Amount spent on expense")
+    parser.add_argument("--id", type=str, help="Expense Id")
+    parser.add_argument("--day", type=str, help="Expense day")
+    parser.add_argument("--month", type=str, help="Expense Month")
+    parser.add_argument("--year", type=str, help="Expense Year")
+
+    args = parser.parse_args()._get_kwargs()
+    command = args[0][1]
+    description = args[1][1]
+    amount = args[2][1]
+    expense_id = args[3][1]
+    day = args[4][1]
+    month = args[5][1]
+    year = args[6][1]
+
+    expense_handler = ExpenseHandler()
+    expenses = expense_handler.read_file()
+    match command:
+        case "add":
+            expense_handler.add_expense(amount, description)
+            print("Expense added successfully.")
+        case "delete":
+            expense_handler.delete_expense(expense_id, expenses)
+            print("Expense deleted successfully.")
+        case "list":
+            expense_handler.list_expenses(expenses)
+        case "summary":
+            total_amount = expense_handler.summary(
+                expenses, day=day, month=month, year=year
+            )
+            print(f"Total amount spent: ${total_amount:.2f}")
 
 
 if __name__ == "__main__":
